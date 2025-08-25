a5256221c9dc8b510370defa34566c53c0764e8c
u8934334 <brian61011@gmail.com>
2025-08-25 11:52:36 +0800

    Update LoadRouter

--- src/LoadRouter.py
+++ src/LoadRouter.py
@@ -7,6 +7,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributed as dist
 
+def print_gpu_usage(tag=""):
+    device = torch.device("cuda:1")
+    allocated = torch.cuda.memory_allocated(device) / (1024 ** 3)  # GB
+    reserved = torch.cuda.memory_reserved(device) / (1024 ** 3)    # GB
+    print(f"[{tag}] GPU memory allocated: {allocated:.3f} GB, reserved: {reserved:.3f} GB")
+
 @dataclass
 class ModelConfig:
     num_hidden_layers: int = 24
@@ -26,49 +32,47 @@ class ModelConfig:
     rope_ntk_alpha: float = 1.0
     rope_ntk_beta: float = 32.0
 
-def random_selection(topk_indices, topk_values, experts_to_select):
-    batch_size = topk_indices.size(0)
-    final_indices_list = []
-    final_values_list = []
-
-    for i in range(batch_size):
-        random_positions = torch.randperm(topk_indices.size(-1), device=topk_indices.torch.device("cuda:1"))[:experts_to_select]
-
-        selected_indices = topk_indices[i, random_positions]
-        selected_values = topk_values[i, random_positions]
-        sorted_values, sorted_original_indices = torch.sort(selected_values, descending=True)
-        sorted_indices = selected_indices[sorted_original_indices]
-
-        final_indices_list.append(sorted_indices)
-        final_values_list.append(sorted_values)
-
-    final_indices = torch.stack(final_indices_list)
-    final_values = torch.stack(final_values_list)
-
-    return final_values, final_indices
-
-
 class LoadRouter(nn.Module):
-    def __init__(self, config: ModelConfig, model_path: str, layer_idx: int, top_k: int, experts_to_select: int):
+    def __init__(self, config, model_path: str, layer_idx: int, top_k: int, experts_to_select: int):
         super().__init__()
-        self.top_k = config.experts_per_token
+        self.top_k = top_k
+        self.random = experts_to_select
         self.num_experts = config.num_experts
         self.hidden_dim = config.hidden_size
         self.model_path = model_path
         self.layer_idx = layer_idx
+
+        router_params = torch.load(f"{self.model_path}/layers/{self.layer_idx}/mlp/router.pt")
+        self.weight = nn.Parameter(router_params['weight'])
+        self.bias = nn.Parameter(router_params['bias'])
+        print_gpu_usage("After loading all expert weights")
         
     def forward(self, hidden_states: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        router_params = torch.load(f"{self.model_path}/layers/{self.layer_idx}/mlp/router.pt")
-        weight = router_params['weight']
-        bias = router_params['bias']
-
         hidden_states = hidden_states.reshape(-1, self.hidden_dim)
-        router_logits = F.linear(hidden_states, weight, bias)
+        router_logits = F.linear(hidden_states, self.weight, self.bias)
         router_top_values, router_top_indices = torch.topk(router_logits, self.top_k, dim=-1)
-        final_top_values, final_top_indices = random_selection(router_top_indices, router_top_values, self.experts_to_select)
+
+        batch_size = router_top_indices.size(0)
+        final_indices_list = []
+        final_values_list = []
+
+        for i in range(batch_size):
+            random_positions = torch.randperm(router_top_indices.size(-1), device=router_top_indices.device)[:self.random]
+
+            selected_indices = router_top_indices[i, random_positions]
+            selected_values = router_top_values[i, random_positions]
+            sorted_values, sorted_original_indices = torch.sort(selected_values, descending=True)
+            sorted_indices = selected_indices[sorted_original_indices]
+
+            final_indices_list.append(sorted_indices)
+            final_values_list.append(sorted_values)
+
+        final_top_indices = torch.stack(final_indices_list)
+        final_top_values = torch.stack(final_values_list)
+
+        #final_top_values, final_top_indices = random_selection(router_top_indices, router_top_values, self.experts_to_select)
         final_top_values = F.softmax(final_top_values, dim=-1, dtype=torch.float32).to(hidden_states.dtype)
         router_scores = torch.zeros_like(router_logits, dtype=hidden_states.dtype)
         router_scores.scatter_(1, final_top_indices, final_top_values)
-        del router_params, weight, bias
 
         return router_scores, final_top_indices
--- src/load.py
+++ src/load.py
@@ -2,10 +2,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 import torch
 import os
 from LoadExperts import ModelConfig, LoadExperts
+from LoadRouter import ModelConfig, LoadRouter
 import time
 
 MODEL_PATH = "/work/morrisliu07/gpt-oss-20b-split/"
 GPU_DEVICE = "cuda:1"
+TOPK = 7
+RANDOM = 4
 
 def print_gpu_usage(tag=""):
     device = torch.device(GPU_DEVICE)
@@ -20,13 +23,26 @@ print_gpu_usage("After initialize model skeleton")
 print(f"Model Initial Structure:\n{model}")
 
 for name, module in model.named_modules():
-    if len(list(module.children())) == 0 and 'mlp.experts' not in name:  # only leaf modules and non-expert layers
+    if len(list(module.children())) == 0 and 'mlp.experts' not in name and 'mlp.router' not in name:  # only leaf modules and non-expert layers
         safe_dir = os.path.join(MODEL_PATH, *name.split('.')[1:-1])  # 使用 / 連接模組名稱
         safe_name = name.split('.')[-1]  # 只保留最後一個名稱部分
         state_dict = torch.load(f"{safe_dir}/{safe_name}.pt", map_location=GPU_DEVICE)
         module.load_state_dict(state_dict)
         print(f"Loaded {name} into the model.")
-print_gpu_usage("After loading non-expert layers")
+print_gpu_usage("After loading non-mlp layers")
+
+# Replace the MLP layers with customized router layers
+for layer_idx, layer in enumerate(model.model.layers):
+    layer.mlp.router = LoadRouter(
+        config=ModelConfig(),
+        model_path=MODEL_PATH,
+        layer_idx=layer_idx,
+        top_k=TOPK,                 
+        experts_to_select=RANDOM
+    )
+model.to(GPU_DEVICE)
+print_gpu_usage("After replacing router layers and moving model to GPU")
+print(f"Model Structure After Replacement:\n{model}")
         
 # Replace the MLP layers with customized MLP layers
 for layer_idx, layer in enumerate(model.model.layers):
